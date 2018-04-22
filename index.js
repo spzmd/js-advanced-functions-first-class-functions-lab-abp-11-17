c2cb8be879a6f3780de38a87e5aec78a13943471
spzmd <b.keithdavid@gmail.com>
2018-04-22 12:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function () {
+  return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function () {
+  return drivers.slice(2);
+}
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
+
+
+
+/*const createFareMultiplier = function (integer) {
+  return integer * createFareMultiplier() ;*/
+
+
+const fareDoubler = function (integer) {
+  return integer * 2;
+};
